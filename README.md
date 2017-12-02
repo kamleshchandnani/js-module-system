6d30c5157a16a95123192e2376c00fd99c2d83ac
kamleshchandnani <kamlesh.chandnani@gmail.com>
2017-12-03 00:57:38 +0530

    Basic setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# What this repository is for?
+This respository has all the common modules which will be used across bizom projects.
---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import filesize from "rollup-plugin-filesize";
+
+const env = process.env.NODE_ENV;
+const pkg = require("./package.json");
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: {
+      es: pkg.module,
+      cjs: pkg.main
+    }[env],
+    format: env
+  },
+  external: ["moment"],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: "node_modules/**",
+      plugins: ["external-helpers"],
+      // if external helpers true then use global babel object
+      externalHelpers: true
+    }),
+    commonjs(),
+    filesize()
+  ]
+};
